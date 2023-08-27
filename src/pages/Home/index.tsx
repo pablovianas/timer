34bfbaa369ac720a099a93bb7639cb826293566f
Pablo Viana <pabloviana2010@gmail.com>
2023-08-27 15:11:54 -0300

    chore: home page styled done

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,10 @@ import {
   CountDownContainer,
   FormContainer,
   HomeContainer,
+  MinutsAmountInput,
   Separator,
+  StartCountDownButton,
+  TaskInput,
 } from './styles'
 export const Home = () => {
   return (
@@ -11,9 +14,25 @@ export const Home = () => {
       <form>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input id="task" type="text" />
+          <TaskInput
+            id="task"
+            placeholder="Dê um nome para seu projeto"
+            list="task-suggestions"
+          />
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+          </datalist>
           <label htmlFor="minutesAmount">durante</label>
-          <input type="number" id="minutesAmount" />
+          <MinutsAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
           <span>minutos .</span>
         </FormContainer>
         <CountDownContainer>
@@ -24,9 +43,9 @@ export const Home = () => {
           <span>0</span>
         </CountDownContainer>
 
-        <button type="submit">
+        <StartCountDownButton type="submit">
           <Play size={24} /> Começar
-        </button>
+        </StartCountDownButton>
       </form>
     </HomeContainer>
   )
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -51,3 +51,56 @@ export const Separator = styled('div')`
   display: flex;
   justify-content: center;
 `
+
+export const StartCountDownButton = styled('button')`
+  width: 100%;
+  border: none;
+  padding: 1rem;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  font-weight: bold;
+  cursor: pointer;
+  background: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme['gray-100']};
+
+  &:disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+
+  &:not(:hover):hover {
+    background: ${(props) => props.theme['green-700']};
+  }
+`
+
+const BaseInput = styled('input')`
+  background: transparent;
+  height: 2.5rem;
+  border: 0;
+  border-bottom: 2px solid ${(props) => props.theme['gray-500']};
+  font-weight: bold;
+  font-size: inherit;
+  padding: 0 0.5rem;
+  flex-wrap: wrap;
+  color: ${(props) => props.theme['gray-100']};
+  &:focus {
+    box-shadow: none;
+    border-color: ${(props) => props.theme['green-500']};
+  }
+  &::placeholder {
+    color: ${(props) => props.theme['gray-500']};
+  }
+`
+export const TaskInput = styled(BaseInput)`
+  flex: 1;
+  &::-webkit-calendar-picker-indicator {
+    display: none !important;
+  }
+`
+
+export const MinutsAmountInput = styled(BaseInput)`
+  width: 4rem;
+`
